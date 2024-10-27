8543c0d29f02651482fe35d9c19fa23bd59b5618
Ashade Samson <ashadesamson99@gmail.com>
2024-10-27 12:51:52 +0100

    [refactor]: Separate non-critical styles to improve page load time

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+
+
+
 function initializeCarousel(){
-  const rootNode = document.querySelector('.project-view')
+
   const viewNode = document.querySelector('.projects-container')
-
-  const prevButtonNode = rootNode.querySelector('.prev')
-  const nextButtonNode = rootNode.querySelector('.next')
-
   const options = { loop: true}
   const emblaApi = EmblaCarousel(viewNode, options)
+
+  const rootNode = document.querySelector('.project-view')
+  const prevButtonNode = rootNode.querySelector('.prev')
+  const nextButtonNode = rootNode.querySelector('.next')
 
   prevButtonNode.addEventListener('click', emblaApi.scrollPrev, false)
   nextButtonNode.addEventListener('click', emblaApi.scrollNext, false)
